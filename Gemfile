d78cb9197f40ccf6bac32488988bff5710d001d6
Mickey <sanchez.mickey@gmail.com>
2014-01-19 16:51:03 -0500

    initial setup

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.15'
+
+gem 'pg'
+
+group :development do
+  gem 'annotate'
+  gem 'pry-rails'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourPlanner::Application.config.secret_token = '962b195bb271634441f61a054ec1df9f064d7b74bb4d712a820f0431c3568a500d0b986dd655beabe7ae27b3089381450b8f611c9e0b1b2845abe01abe51d4e4'
