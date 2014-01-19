4532ed64e4cea085dfbfd35ac6d181017ead50dd
Mickey <sanchez.mickey@gmail.com>
2014-01-19 17:05:13 -0500

    adds events table

---
+++ test/unit/event_test.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class EventTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
