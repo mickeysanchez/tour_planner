bc83e7c8c029952573faccf0fb240d8b569efd28
Mickey <sanchez.mickey@gmail.com>
2014-02-21 14:08:12 -0500

    maps zoom to fit tour

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -23,7 +23,6 @@ describe Venue do
   it { should validate_presence_of(:address) }
   it { should validate_presence_of(:city) }
   it { should validate_presence_of(:state) }
-  it { should validate_presence_of(:zipcode) }
   
   it { should have_many(:events)}
 end
