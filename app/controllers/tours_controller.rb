96d796de52e3c732a717287a2a8208b0730f1432
Mickey <sanchez.mickey@gmail.com>
2014-02-23 15:33:46 -0500

    adds embed link on tour page

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -59,6 +59,6 @@ class ToursController < ApplicationController
     headers['Access-Control-Allow-Methods'] = 'GET'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
     headers['Access-Control-Max-Age'] = "1728000"
-    render json: geo_data(Tour.first)
+    render json: geo_data(Tour.find[params[:id]])
   end
 end
