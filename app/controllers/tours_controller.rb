c9c9b5eb4df9e379ed73ca3d952204fa2dca2242
Mickey <sanchez.mickey@gmail.com>
2014-02-23 15:40:11 -0500

    tweakin access controls for public api

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -57,6 +57,7 @@ class ToursController < ApplicationController
   def map_embed_data
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
     headers['Access-Control-Max-Age'] = "1728000"
     render json: geo_data(Tour.find[params[:id]])
