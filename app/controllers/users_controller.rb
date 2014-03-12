a5a587bf43fd775e9dfc2beb80bf5481f2e067ed
Mickey <sanchez.mickey@gmail.com>
2014-03-12 15:34:24 -0400

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
       redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
-      render :new
+      render 'sessions/new'
     end
   end
   
